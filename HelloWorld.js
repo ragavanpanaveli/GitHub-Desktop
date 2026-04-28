2d7984718b568382b33ed33e37a0fa3a2db63f35
Ragavan <ragavan5710@gmail.com>
2026-04-28 17:01:40 +0530

    update ts file

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,11 +1,12 @@
 "use strict";
 let hasPassword = true;
 let hasUsername = true;
-//let canLogin = hasPassword && hasUsername;
-//console.log("Can login:", canLogin);
+let canLogin = hasPassword && hasUsername;
+console.log("Can login:", canLogin);
 if (hasPassword && hasUsername) {
     console.log("Login in");
 }
 else {
     console.log("can't login in");
 }
+
--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,11 +1,11 @@
 let hasPassword = true;
 let hasUsername = true;
-//let canLogin = hasPassword && hasUsername;
-//console.log("Can login:", canLogin);
+let canLogin = hasPassword && hasUsername;
+console.log("Can login:", canLogin);
 if(hasPassword && hasUsername){
     console.log("Login in");
 }
-else{
+else {
     console.log("can't login in")
 }
 
